54423c9d649d54b3c85682fd1b2c1f99a24aa4cd
morsa <voxfoot@gmail.com>
2022-02-01 21:57:50 +0500

    add new file messages.js

---
+++ src/messages.js
@@ -0,0 +1,5 @@
+const congratulation = (name) => console.log(`Congratulations, ${name}!`);
+export const greetings = () => console.log('Welcome to the Brain Games! ');
+export const helloUser = (name) => console.log(`Hello, ${name}!`);
+
+export default congratulation;
