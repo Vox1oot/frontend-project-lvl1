4c509c45ddcc3d8576d6deab1ff6a39824a689fe
morsa <voxfoot@gmail.com>
2022-02-02 21:35:43 +0500

    delete messages.js

--- src/messages.js
+++
@@ -1,5 +0,0 @@
-const congratulation = (name) => console.log(`Congratulations, ${name}!`);
-export const greetings = () => console.log('Welcome to the Brain Games! ');
-export const helloUser = (name) => console.log(`Hello, ${name}!`);
-
-export default congratulation;
