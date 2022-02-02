d686606b6d34417347301e1ba78002c552740ff0
morsa <voxfoot@gmail.com>
2022-02-02 21:36:32 +0500

    add new dir games

---
+++ src/games/even-game.js
@@ -0,0 +1,14 @@
+import startGame from '../index.js';
+import getRandomNumber from '../random-number.js';
+
+export const ruleIsEven = 'Answer "yes" if the number is even, otherwise answer "no".';
+const isEven = (number) => number % 2 === 0; /* return true ot false */
+
+const generatorIsEven = () => {
+  const currentNumber = getRandomNumber();
+  const correctAnswer = isEven(currentNumber) ? 'yes' : 'no';
+  return [currentNumber, correctAnswer];
+};
+
+startGame(ruleIsEven, generatorIsEven);
+export default generatorIsEven;
