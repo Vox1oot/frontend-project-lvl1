dfc32f3acbd4782eaf44dd012c3449419ff8883e
morsa <voxfoot@gmail.com>
2022-02-04 21:11:38 +0500

    refactor: change logic function

--- src/random-number.js
+++ src/random-number.js
@@ -1,3 +1,3 @@
 /* The function generate ranodm number from 0 to 100 */
-const getRandomNumber = () => Math.floor(Math.random() * 100);
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 export default getRandomNumber;
