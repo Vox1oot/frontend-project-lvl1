f232099e90900149d1251107585770cb5c62e76f
morsa <voxfoot@gmail.com>
2022-02-04 21:56:35 +0500

    docs: change a comment

--- src/random-number.js
+++ src/random-number.js
@@ -1,3 +1,3 @@
-/* The function generate ranodm number from 0 to 100 */
+/* The function generate a random number in the range from min to max */
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 export default getRandomNumber;
