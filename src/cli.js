0ec6f57bd88832e61a3e8283ae6b4ff0b66ad220
morsa <voxfoot@gmail.com>
2022-02-08 22:19:16 +0500

    refactor: change logic cli.js and brain-games.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const getUserName = () => readlineSync.question('May I have your name? ');
-
-export default getUserName;
+export default function greeting() {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
+}
