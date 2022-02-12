2e8c27d5cd7fa1183040b1d142cf0d64ef564e64
morsa <voxfoot@gmail.com>
2022-02-12 16:40:19 +0500

    fix: function isPrime, add check 0 and 1

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -4,19 +4,16 @@ import getRandomNumber from '../random-number.js';
 export const rulePrime = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  const result = true;
-
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return false;
     }
   }
-
-  return result;
+  return number > 1;
 };
 
 const generatorIsPrime = () => {
-  const question = getRandomNumber(3, 50);
+  const question = getRandomNumber(0, 50);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
 
   return [question, correctAnswer];
